b1621d59115f006a652ef6a4fb23901ba7a2d4a7
Michiel Swaanen <michiel.swaanen.gsm@gmail.com>
2020-07-03 17:13:57 +0200

    Updates https requirement for cookies

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -13,7 +13,7 @@ app.set('trust proxy', true); // Allows connection with the ClusterIP
 app.use(json());
 app.use(cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: false //process.env.NODE_ENV !== 'test'
 }));
 app.use(currentUser);
 
