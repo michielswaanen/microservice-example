dc412c36feb18e42f7f7e64820fa824daebb0295
Michiel Swaanen <michiel.swaanen.gsm@gmail.com>
2020-07-04 10:58:18 +0200

    Testing #5

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,54 +1,46 @@
 import Link from "next/link";
 
-const LandingPage = () => {
-    return(
+const LandingPage = ({ currentUser, tickets }) => {
+    const ticketList = <div>Hello</div>
+    // const ticketList = tickets.map((ticket) => {
+    //
+    //     return(
+    //         <tr key={ticket.id}>
+    //             <td>{ticket.title}</td>
+    //             <td>{ticket.price}</td>
+    //             <td>
+    //                 <Link href={'/tickets/[ticketId]'} as={`/tickets/${ticket.id}`}>
+    //                     <a href="" className="nav-link">View</a>
+    //                 </Link>
+    //             </td>
+    //         </tr>
+    //     )
+    // })
+
+    return (
         <div>
-            Testing Landing Page
+            <h1>Tickets</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {ticketList}
+                </tbody>
+            </table>
         </div>
     )
+    return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
-// const LandingPage = ({ currentUser, tickets }) => {
-//     const ticketList = tickets.map((ticket) => {
-//         return(
-//             <tr key={ticket.id}>
-//                 <td>{ticket.title}</td>
-//                 <td>{ticket.price}</td>
-//                 <td>
-//                     <Link href={'/tickets/[ticketId]'} as={`/tickets/${ticket.id}`}>
-//                         <a href="" className="nav-link">View</a>
-//                     </Link>
-//                 </td>
-//             </tr>
-//         )
-//     })
-//
-//     return (
-//         <div>
-//             <h2>Tickets</h2>
-//             <table className="table">
-//                 <thead>
-//                     <tr>
-//                         <th>Title</th>
-//                         <th>Price</th>
-//                         <th>Link</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                 {ticketList}
-//                 </tbody>
-//             </table>
-//         </div>
-//     )
-//     return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
-// }
-//
-// LandingPage.getInitialProps = async (context, client, currentUser) => {
-//     const { data } = await client.get('/api/tickets');
-//
-//     console.log("PROPS", data);
-//
-//     return { tickets: data };
-// }
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+    // const { data } = await client.get('/api/tickets');
+
+    // return { tickets: data };
+}
 
 export default LandingPage;
